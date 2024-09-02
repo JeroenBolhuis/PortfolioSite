f27d4a496e6283a52efb3796800c7336edbc5ca2
JeroenBolhuis <j.bolhuis@student.avans.nl>
2024-09-02 18:36:16 +0200

    ya

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './resources/views/**/*.blade.php',  // Adjust the path if necessary
+    "./Components/**/*.{js,ts,jsx,tsx}",
     './resources/css/**/*.css',          // Adjust the path if necessary
     // Add other paths if you have components or JS files using Tailwind classes
   ],
