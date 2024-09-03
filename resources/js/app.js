53cf8a09df0ebbf05860d2abb1a0ca3b2823b4a6
JeroenBolhuis <j.bolhuis@student.avans.nl>
2024-09-04 01:59:05 +0200

    put styling from js to css and finetuned google popup with more breakpoints

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import imagesLoaded from 'imagesloaded';
+import { fromJSON } from 'postcss';
 
 document.addEventListener('DOMContentLoaded', function() {
     var grid = document.querySelector('.masonry-grid');
@@ -14,36 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
     function initializeCalendarButton() {
         var target = document.getElementById('calendar-container');
         if (target && window.calendar && window.calendar.schedulingButton) {
-            var label = 'Make an appointment'; // You might want to handle translations differently
+            var label = target.dataset.appointmentLabel || 'Make an appointment';
             window.calendar.schedulingButton.load({
                 url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3L2W-jtuGZGEAPef1y57HPVol19JEf08SWMKHsTp2jKdEhhKvirag_iTLSujEQrZmAGcb1w_O7?gv=true',
                 color: '#039BE5',
                 label: label,
                 target: target,
             });
-
-            // After the button is loaded, apply Tailwind-like styles
-            setTimeout(() => {
-                const button = document.querySelector('.qxCTlb');
-                var changedstyles = false;
-                while (!changedstyles) {
-                    if (button) {
-                        button.classList.add('bg-gradient-to-r', 'from-purple-800', 'to-green-500', 'hover:from-purple-700', 'hover:to-green-400', 'text-white', 'font-bold', 'rounded', 'transform', 'transition', 'hover:scale-105', 'duration-300', 'ease-in-out', 'md:text-lg', 'text-wrap', 'w-full', 'md:w-auto');
-                        button.style.setProperty('padding', '1rem 1.5rem', 'important');
-                        changedstyles = true;
-                    }
-                }                        // Add an event listener for the 'click' event
-                button.addEventListener('click', function() {            
-                    setTimeout(() => {
-                        const cont = document.querySelector('.hur54b');
-                        if (cont) {
-                            if (window.innerWidth < 768) { // only change padding on mobile
-                                cont.style.setProperty('padding', '5rem 0.75rem 2rem 0.75rem', 'important');
-                            }
-                        }
-                    }, 0);
-                });
-            }, 0); // Small delay to ensure button is rendered
         } else {
             // If the calendar script hasn't loaded yet, try again in a moment
             setTimeout(initializeCalendarButton, 100);
