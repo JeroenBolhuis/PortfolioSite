1a2cd74bcd3ab205de5b6f6179c0d4b1838330e2
JeroenBolhuis <j.bolhuis@student.avans.nl>
2025-02-14 00:34:47 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', __('EfficiënC'))</title>
+    <title>@yield('title', __('Jeroen Bolhuis'))</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
 </head>
